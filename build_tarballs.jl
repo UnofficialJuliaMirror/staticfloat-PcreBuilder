0dfc959da1cac3270a574a6a7b6ee70d1e552934
staticfloat@gmail.com <staticfloat@gmail.com>
2018-03-20 00:22:19 -0700

    Initial commit

---
+++ build_tarballs.jl
@@ -0,0 +1,42 @@
+using BinaryBuilder
+
+# Collection of sources required to build libffi
+sources = [
+    "http://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.41.tar.gz" =>
+    "244838e1f1d14f7e2fa7681b857b3a8566b74215f28133f14a8f5e59241b682c",
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd pcre-8.41/
+./configure --prefix=$prefix --host=$target
+make -j${nproc}
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    BinaryProvider.Linux(:i686, :glibc),
+    BinaryProvider.Linux(:x86_64, :glibc),
+    BinaryProvider.Linux(:aarch64, :glibc),
+    BinaryProvider.Linux(:armv7l, :glibc),
+    BinaryProvider.Linux(:powerpc64le, :glibc),
+    BinaryProvider.MacOS(),
+    BinaryProvider.Windows(:i686),
+    BinaryProvider.Windows(:x86_64)
+]
+
+# The products that we will ensure are always built
+products(prefix) = [
+    LibraryProduct(prefix, "libpcre", :libpcre)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, "pcre", sources, script, platforms, products, dependencies)
+
